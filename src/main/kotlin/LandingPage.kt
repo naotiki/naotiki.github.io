3b588d15a651da643026beb4fb9373dad7313b02
Naotiki <naotiki@naotiki-apps.xyz>
2022-08-11 18:10:06 +0900

    Update TopPage and fix ./gradlew permission

--- src/main/kotlin/LandingPage.kt
+++ src/main/kotlin/LandingPage.kt
@@ -1,15 +1,19 @@
-import csstype.AlignItems
-import csstype.TextAlign
-import csstype.px
+import csstype.*
+import emotion.react.css
 import mui.icons.material.GitHub
 import mui.icons.material.Twitter
 import mui.material.*
+import mui.material.Size
 import mui.material.styles.TypographyVariant
 import mui.system.responsive
 import mui.system.sx
+import muix.pickers.DateTimePickerProps
+import org.w3c.dom.HTML.div
 import react.FC
 import react.Props
+import react.dom.html.ReactHTML.div
 import react.useState
+import kotlin.js.Date
 
 external interface WelcomeProps : Props {
     var name: String
@@ -17,13 +21,9 @@ external interface WelcomeProps : Props {
 
 val LandingPage = FC<WelcomeProps> { props ->
     var name by useState(props.name)
-
     Box {
-
         Stack {
             sx {
-                // display= Display.flex
-                //  justifyContent = JustifyContent.center
                 alignItems = AlignItems.center
             }
             Avatar {
@@ -31,11 +31,8 @@ val LandingPage = FC<WelcomeProps> { props ->
                 sx {
                     width = 256.px
                     height = 256.px
-
                 }
             }
-
-
             Typography {
                 variant = TypographyVariant.h3
                 sx { textAlign = TextAlign.center }
@@ -45,11 +42,13 @@ val LandingPage = FC<WelcomeProps> { props ->
                 direction = responsive(StackDirection.row)
                 spacing = responsive(2)
                 Fab {
+                    href = "https://twitter.com/NaotikiKt"
                     size = Size.small
                     color = FabColor.primary
                     Twitter()
                 }
                 Fab {
+                    href = "https://github.com/naotiki"
                     size = Size.small
                     color = FabColor.primary
                     GitHub()
@@ -58,6 +57,23 @@ val LandingPage = FC<WelcomeProps> { props ->
             }
 
         }
+        div{
+           css {
+               width=100.px
+               height=30.px
+
+           }
+        }
+        Typography {
+            variant = TypographyVariant.h4
+
+            +"README"
+        }
+        Typography {
+            +"Kotlinが大好きな高専生です"
+            +Date(Date.now()).toDateString()
+        }
+
     }
 
 }
